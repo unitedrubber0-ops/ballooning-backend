74d086ffc3391f0977cd9577486f930b9a82fdf2
EXP-24 <EXP-24@URI.COM>
2025-08-18 20:03:52 +0530

    Fix: Configure vite for react plugin

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-pdf'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/react-pdf/, /pdfjs-dist/],
-    },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://ballooning-backend.onrender.com',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
 })
