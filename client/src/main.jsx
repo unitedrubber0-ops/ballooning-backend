c5232ea7b76d31d97dea206587575c22fded7e0f
EXP-24 <EXP-24@URI.COM>
2025-08-18 19:57:44 +0530

    Fix: Correct component import casing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./app.jsx";;
 import "./styles.css";
 
 const root = createRoot(document.getElementById("root"));
